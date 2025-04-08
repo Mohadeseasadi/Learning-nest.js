7c5299d6580f5ae95e9868b7570be848c93836fa
mohadese <mohiw.1380@gmail.com>
2025-04-08 17:43:06 +0330

    first step
    added express

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // const app = await NestFactory.create<NestExpressApplication>(AppModule); ---> add express
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
